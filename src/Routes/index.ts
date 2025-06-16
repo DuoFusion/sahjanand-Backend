35a8d3aae4b88e82020da761a5f45318d015845d
Pramit Mangukiya <pramitmangukiya602@gmail.com>
2025-06-16 12:20:58 +0530

    typescript changes

---
+++ src/Routes/index.ts
@@ -0,0 +1,14 @@
+"use strict"
+import { Request, Router, Response } from 'express'
+// import { userRouter } from './user'
+import { userStatus } from '../common'
+// import { userRouter } from './user'
+
+
+
+const router = Router()
+
+// router.use('/user',  accessControl, userRouter)
+
+
+export { router }
