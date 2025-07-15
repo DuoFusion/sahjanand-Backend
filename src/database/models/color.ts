4f5a68a504e31cb66eb2c11e267461ceec24c853
Pramit Mangukiya <pramitmangukiya602@gmail.com>
2025-07-15 11:31:24 +0530

    color code add

--- src/database/models/color.ts
+++ src/database/models/color.ts
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 
 const colorSchema = new mongoose.Schema({
     name: { type: String },
+    colorCode: { type: String },
     priority: { type: Number, default: 1 },
     isDeleted: { type: Boolean, default: false },
     isBlocked: { type: Boolean, default: false }
--- src/database/models/product.ts
+++ src/database/models/product.ts
@@ -12,35 +12,22 @@ const productSchema = new mongoose.Schema({
     subCategoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'category' },
     tags: [{ type: String }],
     attributes: {
-        color: [{ type: String }],
-        size: [{ type: String }],
-        material: [{ type: String }],
-        fabric: [{ type: String }],
-        occasion: [{ type: String }]
+        colorIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'color' }],
+        sizeIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'size' }],
+        materialIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'material' }],
+        fabricIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'fabric' }],
+        occasionIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'occasion' }]
     },
     stock: { type: Number, default: 0 },
     isNewArrival: { type: Boolean, default: false },
     isBestSelling: { type: Boolean, default: false },
     isFeatured: { type: Boolean, default: false },
     showOnHomepage: { type: Boolean, default: false },
-    seo: {
-        metaTitle: { type: String },
-        metaDescription: { type: String },
-        keywords: [{ type: String }]
-    },
-    rating: { type: Number, default: 0 },
-    reviews: [{
-        user: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
-        rating: { type: Number },
-        comment: { type: String },
-        date: { type: Date}
-    }],
     isDeleted: { type: Boolean, default: false },
     isBlocked: { type: Boolean, default: false },
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
     updatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'user' }
 }, { timestamps: true, versionKey: false });
-
 
 productSchema.index({ name: 'text', description: 'text', tags: 'text' });
 
