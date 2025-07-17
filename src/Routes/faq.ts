6a00fffdcc23aae032cc71c9b08f8593ac8d362a
Pramit Mangukiya <pramitmangukiya602@gmail.com>
2025-07-17 16:24:53 +0530

    banner and faq API changes

--- src/Routes/faq.ts
+++ src/Routes/faq.ts
@@ -1,15 +1,16 @@
 import express from 'express';
 import { faqController } from "../controllers";
+import { adminJWT, userJWT } from '../helper';
 
 const router = express.Router();
 
-// Admin routes
+router.use(userJWT)
+router.get('/', faqController.get_all_faqs)
+
+router.use(adminJWT)
 router.post('/add', faqController.add_faq)
 router.post('/edit', faqController.edit_faq)
 router.delete('/:id', faqController.delete_faq)
-
-// Public routes
-router.get('/', faqController.get_all_faqs)
 router.get('/:id', faqController.get_faq_by_id)
 
 export const faqRoutes = router; 
