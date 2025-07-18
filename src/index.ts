d81b5f28efa719732daa3539214c5cba4e5b6b56
Pramit Mangukiya <pramitmangukiya602@gmail.com>
2025-07-18 18:16:48 +0530

    webp image add

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,8 @@ const fileFilter = (req, file, cb) => {
     if (
         file.mimetype === "image/png" ||
         file.mimetype === "image/jpg" ||
-        file.mimetype === "image/jpeg"
+        file.mimetype === "image/webp" ||
+        file.mimetype === "image/jpeg" 
     ) {
         cb(null, true);
     } else {
